e6caee20bc59a0e5cda793b741688231291b5a0d
apple <apple@appleui-MacBookAir.local>
2026-07-14 11:42:20 +0900

    코멘트 추가

--- calculator.js
+++ calculator.js
@@ -1,14 +1,21 @@
+// 덧셈
 const add = (a, b) => {
   return a + b;
 };
+
+// 뺄셈
 const substract = (a, b) => {
   return a - b;
 };
 
+
+// 곱셈
 const multiply = (a, b) => {
   return a * b;
 };
 
+
+// 나눗셈
 const divide_free = (a, b) => {
   return a / b;
 };
